a1eaae40cce8d8c4848126703b4c70336258df58
tmushayahama <tmushayahama@gmail.com>
2014-07-07 21:23:12 -0400

    Now outputing the sorted occurrence, but needs improvements. Added the comparator, however for the descending, I heard to tweak and multiply by -1. A better solution will come

--- src/OccurrenceCalculator.java
+++ src/OccurrenceCalculator.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * OccurrenceCalculator deals with file handling and sorting the text occurrence
@@ -12,6 +14,36 @@ import java.util.Map;
  */
 public class OccurrenceCalculator {
 	/**
+	 *  A comparator class to help sort the values of the occurrence map. It sorts
+	 *  by overriding the the compare function to sort by comparing the values of the map.
+	 *
+	 */
+	static class OccurrenceComparator implements Comparator<String> {
+
+        Map<String, Integer> occurrenceMapBase;
+
+        OccurrenceComparator(Map<String, Integer> occurrenceMapBase) {
+            this.occurrenceMapBase = occurrenceMapBase;
+        }
+        /**
+         * Compares the values of a map. If the values are equal, it then compares the keys
+         * For descending order, a quick approach was to multiply by -1. Needs some improvement
+         * 
+         *  @param a the first value to be compared
+         *  @param b the second value to be compared
+         */
+        @Override
+        public int compare(String a, String b) {
+        	
+        	Integer firstInt = this.occurrenceMapBase.get(a);
+        	Integer secondInt = this.occurrenceMapBase.get(b);
+        	 if (firstInt.equals(secondInt)) {
+                 return a.compareTo(b)*-1;
+             }
+        	 return firstInt.compareTo(secondInt)*-1;
+        }
+    }
+	/**
 	 * The reader associated with the input text file.
 	 */
 	BufferedReader reader;
@@ -19,7 +51,8 @@ public class OccurrenceCalculator {
 	 * The storage of occurrence of strings
 	 */
 	Map<String, Integer> occurrenceMap;
-
+     OccurrenceComparator occurrenceComparator;
+     Map<String, Integer> sortedOccurrenceMap;
 	/**
 	 * Constructor assigns the reader
 	 * 
@@ -29,12 +62,15 @@ public class OccurrenceCalculator {
 	public OccurrenceCalculator(BufferedReader reader) {
 		this.reader = reader;
 		this.occurrenceMap = new HashMap<String, Integer>();
+		this.occurrenceComparator = new OccurrenceComparator(occurrenceMap);
+		this.sortedOccurrenceMap = new TreeMap<String, Integer>(this.occurrenceComparator);
 	}
 
 	/**
 	 *  Populates the occurrenceMap by reading string per line and putting it to the map
+	 * @throws IOException 
 	 */
-	public void populateOccurrenceMap() {
+	public void populateOccurrenceMap() throws IOException {
 		String line;
 		try {
 			while ((line = this.reader.readLine()) != null) {
@@ -43,6 +79,8 @@ public class OccurrenceCalculator {
 		} catch (IOException e) {
 			System.out.println("Error reading the file");
 			System.exit(1);
+		} finally {
+            reader.close();
 		}
 	}
 
@@ -63,8 +101,9 @@ public class OccurrenceCalculator {
 	 * Checking before sorting
 	 */
 	public void outputOccurrenceMap() {
-		for(String key: this.occurrenceMap.keySet())
-			System.out.println(key+": "+this.occurrenceMap.get(key));
+		this.sortedOccurrenceMap.putAll(this.occurrenceMap);
+		for(String key: this.sortedOccurrenceMap.keySet())
+			System.out.println(key+": "+this.sortedOccurrenceMap.get(key));
 	}
 
 }
